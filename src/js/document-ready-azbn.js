6c2d6c766ff95fca3437e1fffc3c1317a02e9f89
azbn <i@azbn.ru>
2017-07-28 12:24:13 +0300

    Building-page slider

--- src/js/document-ready-azbn.js
+++ src/js/document-ready-azbn.js
@@ -29,6 +29,108 @@
 	
 	$(function(){
 		
+		$('.azbn__property-gallery').each(function(index){
+			
+			var block = $(this);
+			var move_me = block.find('.azbn__property-gallery__move-me');
+			var items_full = block.find('.azbn__property-gallery__item_full');
+			var items_sm = move_me.find('.azbn__property-gallery__item_sm');
+			var btn_next = block.find('.azbn__property-gallery__next');
+			var btn_prev = block.find('.azbn__property-gallery__prev');
+			
+			if(items_sm.length) {
+				
+				items_sm.each(function(_index){
+					
+					var btn = $(this);
+					var _pos = btn.position().top;
+					
+					btn
+						.attr('data-position-top', _pos)
+						.attr('data-item-index', (_index +1))
+					;
+					
+				});
+				
+				items_sm.on('click.azbn', function(event){
+					event.preventDefault();
+					
+					var btn = $(this);
+					var photo_id = btn.attr('data-photo-id') || 0;
+					var full = items_full.filter('[data-photo-id="' + photo_id + '"]').eq(0);
+					
+					if(!screenJS.isXS()) {
+						
+						var _pos = parseInt(btn.attr('data-position-top'));
+						var _btn_index = parseInt(btn.attr('data-item-index'));
+						
+						var _move_me_h = move_me.outerHeight(true);
+						var _move_me_p_h = move_me.parent().outerHeight(true);
+						var _item_h = btn.outerHeight(true);
+						
+						var _sum_h = _btn_index * _item_h;
+						
+						if((_move_me_h - _pos + _item_h) > _move_me_p_h) {
+							move_me
+								.attr('data-translate-y', _pos)
+								.css({
+									'transform' : 'translateY(-' + _pos + 'px)',
+								})
+							;
+						}
+						
+					}
+					
+					items_sm.filter('.is--active').removeClass('is--active');
+					btn.addClass('is--active');
+					
+					items_full.filter('.is--active').removeClass('is--active');
+					full.addClass('is--active');
+					
+				});
+				
+				btn_next.on('click.azbn', function(event){
+					event.preventDefault();
+					
+					var btn = $(this);
+					
+					var _next = items_sm.filter('.is--active').next();
+					
+					if(_next.length) {
+						
+						_next.trigger('click.azbn');
+						
+					} else {
+						
+						items_sm.eq(0).trigger('click.azbn');
+						
+					}
+					
+				});
+				
+				btn_prev.on('click.azbn', function(event){
+					event.preventDefault();
+					
+					var btn = $(this);
+					
+					var _prev = items_sm.filter('.is--active').prev();
+					
+					if(_prev.length) {
+						
+						_prev.trigger('click.azbn');
+						
+					} else {
+						
+						items_sm.eq(-1).trigger('click.azbn');
+						
+					}
+					
+				});
+				
+			}
+			
+		});
+		
 		$(document.body).on('submit', '.azbn-api-formsave', {}, function(event){
 			event.preventDefault();
 			
